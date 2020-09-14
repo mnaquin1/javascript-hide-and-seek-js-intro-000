12a7d0e8f0677e9dac3a2967ecd62d843dc04a1c
Michael Naquin <mnaquin1@yahoo.com>
2020-09-14 17:01:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function nestedTarget(){
 function deepestChild(){
   return document.querySelector('#grand-node div div div div')
 }
+
+function increaseRankBy(n){
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+ 
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + 1).toString();
+}
+}
