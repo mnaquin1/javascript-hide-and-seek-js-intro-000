388df95f540f0dc5f650354908f57a9ff87ed367
Michael Naquin <mnaquin1@yahoo.com>
2020-09-14 16:55:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.getElementById('nested').querySelector('div.target')
 }
+
+
+function deepestChild(){
+  return document.getElementById('grand-node').querySelector('div div div div')
+}
