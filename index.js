28821fb50cc470654c4b74edc565efbec5b1ff0c
Michael Naquin <mnaquin1@yahoo.com>
2020-09-14 17:08:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('.target')
+}
+
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n){
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+
+for (let i = 0; i < lis.length; i++) {
+lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+}
+}
